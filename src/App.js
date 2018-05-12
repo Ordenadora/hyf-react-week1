8a147b46bc88416c2139bb5bca72644b03785362
Nada <nnadabblan@gmail.com>
2018-05-13 00:10:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function Content() {
-  return (
-    <p ClassName="App-intro">
-    Hello World
-    </p>
-  );
-}
+import Header from './Header.js';
+import Content from './Content';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-      
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Content/>
+        <Header />
+        <Content />
       </div>
     );
   }
 }
+/*
+function Header() {
+  return (
+    <header className="App-header">
+    <img src={logo} className="App-Logo" alt="Logo" />
+    <h1 className="App-title">Crystal Chakra</h1>
+    
+    </header>
+  );
+}
 
+function App() {
+  return (
+    <div className="App">
+    <Header>
+      My Awesome Company
+      </Header>
+      <Content />
+      </div>
+  );
+}
+*/
 export default App;
+
