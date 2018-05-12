3d263bdf4c4019b4eb5b19d4cb44591db22e934d
Nada <nnadabblan@gmail.com>
2018-05-13 00:12:51 +0200

    Create Content.js

---
+++ src/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Content.css';
+import logo from './logo.svg';
+import photo1 from '../photos/week1React/jewelrymake.jpg';
+
+function Content() {
+    return (
+        <p ClassName="Content-intro">
+        I am trying to turn my hobby into business as a jewelry maker.
+        I work with silver, sterling silver, stones and crystals.
+        </p>
+    );
+}
+
+export default Content;
